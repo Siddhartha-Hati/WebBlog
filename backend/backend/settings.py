a72c1e34dc4a7b26599d5cf92bb67f2e68d74d76
Siddhartha-Hati <134580730+Siddhartha-Hati@users.noreply.github.com>
2024-08-16 23:03:04 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-b(wrtrry$+n$7tke9tibyf9hh$sk91)62khysq=5dqzi#)4nin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','dreamwaveinnovation.com']
 # 'localhost', '127.0.0.1','dreamwaveinnovation.com'
 
 # Application definition
