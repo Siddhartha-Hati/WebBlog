5ece22dd5461767ef4b7316c0804cc5194b76300
Siddhartha-Hati <134580730+Siddhartha-Hati@users.noreply.github.com>
2024-08-16 23:31:21 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 ]
 
 
-ROOT_URLCONF = "backend.backend.urls"
+ROOT_URLCONF = "backend.urls"
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "https://dreamwaveinnovation.com",
